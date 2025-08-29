09788cb683eba25faf8510b6c5ff0522ca8b8d99
flanegociosonline-cell <flanegociosonline@gmail.com>
2025-08-29 18:49:48 -0300

    Sitemap automático com coleção aprimorada

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,16 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPassthroughCopy({ 'src/posts/assets': 'assets' });
-
-    eleventyConfig.addCollection("posts", function(collectionApi) {
-        return collectionApi.getFilteredByGlob("src/posts/**/*.njk");
-    });
     
+    // Coleção que busca todas as páginas, incluindo posts
+    eleventyConfig.addCollection("all_pages", function(collection) {
+      return collection.getFilteredByGlob([
+        "src/**/*.njk",
+        "src/**/*.md",
+        "src/**/*.html",
+      ]);
+    });
+
     eleventyConfig.addFilter("rssDate", (date) => pluginRss.rssDate(date));
     
     return {
