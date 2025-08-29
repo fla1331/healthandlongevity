570112f2c0b6d5ca75531050f17c9441f1116da5
flanegociosonline-cell <flanegociosonline@gmail.com>
2025-08-29 14:16:09 -0300

    aff

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
   eleventyConfig.addPassthroughCopy("./src/sitemap_assets");
+  eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
 
   eleventyConfig.addFilter("date", (dateObj) => {
     if (dateObj) {
