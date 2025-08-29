c8bb011ab6b744072cc1895960a009648fa3a1e0
flanegociosonline-cell <flanegociosonline@gmail.com>
2025-08-29 11:13:47 -0300

    Corrige sitemap do Rank Math

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-  // Copia a pasta inteira de assets e o robots.txt
-  eleventyConfig.addPassthroughCopy("./src/assets");
-  eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  // Copia a pasta inteira de assets, o robots.txt e a pasta do sitemap
+  eleventyConfig.addPassthroughCopy({ "./src/assets/": "assets" });
+  eleventyConfig.addPassthroughCopy({ "./src/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "./src/assets/uploads/rank-math/": "assets/uploads/rank-math" });
 
   // Adiciona o filtro de data para ser usado nos templates
   eleventyConfig.addFilter("date", (dateObj) => {
