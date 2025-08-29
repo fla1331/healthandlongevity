cac574e95c92acc0a33ba84526b3dbbfaad98718
flanegociosonline-cell <flanegociosonline@gmail.com>
2025-08-28 21:37:18 -0300

    pasta docs

--- .eleventy.js
+++ .eleventy.js
@@ -23,7 +23,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "dist",
+      output: "docs",
       includes: "_includes",
       data: "_data"
     }
