8b8302629090466e2b10a20fd3330b70902d4d29
flanegociosonline-cell <flanegociosonline@gmail.com>
2025-08-29 12:52:46 -0300

    Adiciona o main-sitemap.xsl e corrige o sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,16 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-  // Copia a pasta inteira de assets, incluindo a pasta rank-math
+  // Copia a pasta inteira de assets
   eleventyConfig.addPassthroughCopy("./src/assets");
 
-  // Copia o robots.txt para a raiz do site.
+  // Copia a nova pasta do sitemap
+  eleventyConfig.addPassthroughCopy("./src/sitemap");
+
+  // Copia o robots.txt
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
-  
-  // Garante que o sitemap.njk seja processado e gerado como sitemap.xml
-  eleventyConfig.addPassthroughCopy("./src/sitemap.njk");
 
-  // Adiciona uma regra explícita para o arquivo de estilo do sitemap
-  eleventyConfig.addPassthroughCopy("./src/assets/uploads/rank-math/main-sitemap.xsl");
-
-  // Adiciona o filtro de data para ser usado nos templates
+  // Adiciona o filtro de data
   eleventyConfig.addFilter("date", (dateObj) => {
     if (dateObj) {
       const date = DateTime.fromISO(dateObj, { zone: "utc" });
