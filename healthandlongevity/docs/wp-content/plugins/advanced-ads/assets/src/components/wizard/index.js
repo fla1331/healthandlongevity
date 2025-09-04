d97addffab67e094e3e25ccdff677a67409dd55d
fla1331 <flanegociosonline@gmail.com>
2025-09-04 09:17:38 -0300

    Delete healthandlongevity/docs directory

--- healthandlongevity/docs/wp-content/plugins/advanced-ads/assets/src/components/wizard/index.js
+++
@@ -1,2 +0,0 @@
-export { default as useWizard } from './useWizard';
-export { default as Wizard } from './Wizard';
--- healthandlongevity/docs/wp-content/plugins/advanced-ads/assets/src/constants.js
+++
@@ -1,4 +0,0 @@
-export default {
-	spinnerHTML:
-		'<span class="spinner advads-ad-parameters-spinner advads-spinner"></span>',
-};
